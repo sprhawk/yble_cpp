f998a38ba18479d07ca20289761fc484fb6734b6
sprhawk <hongbo@yang.me>
2015-05-08 12:13:56 +0800

    added android projects

---
+++ android/yblecpp/src/me/yang/lib/yblecpp/YbleCentral.java
@@ -0,0 +1,14 @@
+package me.yang.lib.yblecpp;
+
+public class YbleCentral {
+	public void startScan_Java(String [] services, int options) {
+		
+	}
+	
+	public void stopScan_Java() {
+		
+	}
+	static {
+		System.loadLibrary("yblecpp");
+	}
+}
---
+++ android/yblecppdemo/src/me/yang/yblecppdemo/YbleActivity.java
@@ -0,0 +1,15 @@
+package me.yang.yblecppdemo;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class YbleActivity extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
